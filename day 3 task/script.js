27c9c94477c3f370ad96c623bcc2c52a787d708f
manojprabhakar24 <118879387+manojprabhakar24@users.noreply.github.com>
2022-11-30 06:37:49 -0800

    Add files via upload

---
+++ day 3 task/script.js
@@ -0,0 +1,79 @@
+
+1)
+for loop;
+ var arr = [ 10, 20, 30, 40, 50] 
+for (var i=0; i < arr.length; i++) {
+   console.log(arr[i])
+}
+output;
+ 10
+ 20
+ 30
+ 40
+ 50
+
+for of;
+var arr = [ 10, 20, 30, 40, 50] 
+for (var obj of arr){
+   console.log(obj.area)
+}
+output;
+10
+20
+30
+40
+50
+
+for in;
+var arr = [ 10, 20, 30, 40, 50] 
+for (var  index in arr){
+   console.log(index.area)
+}
+output;
+10
+20
+30
+40
+50
+
+...................................
+
+
+2)
+
+
+var resume = {
+    "Name" : "manoj",
+ "age" : "25",
+   "gender":"male",
+"qualification":["10th","12th","degree"],
+"location":"tenkasi",
+"experince":"2 years of experince in xyz company",
+language known":["tamil","english"],
+}
+console.log(resume);
+output:
+ Name
+: 
+"manoj"
+age
+: 
+"25"
+experince
+: 
+"2 years of experince in xyz company"
+gender
+: 
+"male"
+language known
+: 
+(2) ['tamil', 'english']
+location
+: 
+"tenkasi"
+qualification
+: 
+(3) ['10th', '12th', 'degree']
+........................................................
+
+
